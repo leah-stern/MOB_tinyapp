5169d928b2cd467debcad2bac7d946e0d9a2ffd6
Leah Stern <leah.stern@tufts.edu>
2017-09-22 18:11:08 -0400

    tinyapp commit

---
+++ app/src/main/java/com/example/leahstern/tinyapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.leahstern.tinyapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void nextPage(View view) {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/leahstern/tinyapp/MainActivity2.java
@@ -0,0 +1,16 @@
+package com.example.leahstern.tinyapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
